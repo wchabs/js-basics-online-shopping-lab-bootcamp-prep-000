1bb1c4d088a3d321e69a86992062516d950b156a
William Chabla <w_chabla@hotmail.com>
2018-03-10 05:45:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,7 +47,10 @@ function total() {
 function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart.splice()
+      cart.splice(i, 1);
+      return cart;
+    } else {
+      return "That item is not in your cart.";
     }
   }
 }
