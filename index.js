0e14e28b3b245cdb7b4ac461ac36ae60738803f2
William Chabla <w_chabla@hotmail.com>
2018-03-10 06:24:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   console.log(cardNumber);
+  console.log(typeof cardNumber);
   if (typeof cardNumber === undefined) {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
