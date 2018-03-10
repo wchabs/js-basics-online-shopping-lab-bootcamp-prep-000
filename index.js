e1a92339420441b3a497cba4fbd43e67ba3d303e
William Chabla <w_chabla@hotmail.com>
2018-03-10 05:19:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,30 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.push({itemName: item, itemPrice: Math.floor(Math.random()*100 + 1)});
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length > 0) {
+    var report = "In your cart, you have";
+    if (cart.length === 1) {
+      report = `${report} ${cart[0].itemName} at $${cart[0].itemPrice}.`;
+      return report;
+    } else if (cart.length === 2) {
+      report = `${report} ${cart[0].itemName} at $${cart[0].itemPrice} and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
+      return report;
+    } else {
+      for (var i = 0; i < cart.length; i++) {
+        if (i === cart.length - 1) {
+          report = `${report} and ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+          return report;
+        } else {
+          report = `${report} ${cart[i].itemName} at $${cart[i].itemPrice},`;
+        }
+      }
+    }
+  } else return "Your shopping cart is empty.";
 }
 
 function total() {
