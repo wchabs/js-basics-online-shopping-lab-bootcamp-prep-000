485c523db0aa01c5e1e08146812baf6cf25d756c
William Chabla <w_chabla@hotmail.com>
2018-03-10 06:05:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber === null) {
+  if (cardNumber == null) {
     console.log("Sorry, we don't have a credit card on file for you.");
   } else {
     var grandTotal = total();
